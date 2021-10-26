b706e334ea5842b74018b5c7f84ba2a34afadcf2
Priyansh Sarvehi <priyanshsarveh@gmail.com>
2021-10-27 03:38:23 +0530

    new

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum {
     public static void main(String[] args){
         int a =12;
         int b =22;
-        System.out.println(a+b);
+        System.out.println("answer+ "a+b);
     }
 }
